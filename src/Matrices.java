367e1496decbbbd6ef9bf92857ee0d8a091d33a4
KRON05FD <andresnitolamoreno@gmail.com>
2023-09-26 21:42:36 -0500

    Inicial
    Se inicia el codigo de la actividad3

--- src/Matrices.java
+++ src/Matrices.java
@@ -1,17 +1,18 @@
+
 import java.util.Random;
 import javax.swing.JOptionPane;
 
 public class Matrices {
+
     public static void main(String[] args) {
         // Solicitamos el numero de filas y columnas que tendran las matrices y se asigna como N para filas y M para las columnas
         int N = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el numero de filas:",
-                        "Matrices", JOptionPane.QUESTION_MESSAGE));
+                "Matrices", JOptionPane.QUESTION_MESSAGE));
         int M = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el numero de columnas:",
-                        "Matrices", JOptionPane.QUESTION_MESSAGE));
+                "Matrices", JOptionPane.QUESTION_MESSAGE));
 
         // Aqui declaramos las tres matrices que vamos a necesitar las dos que se multiplicaran y la tercera que sera el resultadoasignando
         // les asignamos el tamaño que se capturo en N y M
-        
         int[][] matriz1 = new int[N][M];
         int[][] matriz2 = new int[N][M];
         int[][] resultado = new int[N][M];
@@ -37,18 +38,18 @@ public class Matrices {
         // usamos \n para dar los saldos de linea y organizar mejor la impresion
         StringBuilder resultadoStr = new StringBuilder("Matriz 1:\n");
         resultadoStr.append(imprimirMatriz(matriz1));
-        
+
         resultadoStr.append("x\n");
-        
+
         resultadoStr.append("Matriz 2:\n");
         resultadoStr.append(imprimirMatriz(matriz2));
-        
+
         resultadoStr.append("=\n");
-        
+
         resultadoStr.append(imprimirMatriz(resultado));
-        
+
         JOptionPane.showMessageDialog(null, resultadoStr.toString(),
-                        "Resultado de la Multiplicacion", JOptionPane.QUESTION_MESSAGE);
+                "Resultado de la Multiplicacion", JOptionPane.QUESTION_MESSAGE);
     }
 
     // Esta funcion imprime la matriz la recorre con dos ciclos, se agregan los corchetes y los saltos de linea
